bd7c5f79cfc55038e3c4ac83f5e84fe72d220ab8
Josh Gottschall <jjg6989@rit.edu>
2023-04-15 12:27:08 -0400

    fixed  service names

--- http_score_nginx.py
+++ http_score_nginx.py
@@ -12,22 +12,22 @@ def score_NGINX(queue, alive, lock, target, port, value, team_num):
             # We got a 200 response code
             if res.ok:
                 lock.acquire()
-                queue.put({'service': 'HTTP', 'status': 'UP', 'host': target, 'value': value, 'team_num': team_num})
+                queue.put({'service': 'NGINX', 'status': 'UP', 'host': target, 'value': value, 'team_num': team_num})
                 lock.release()
             # We got a non-200 response code
             else:
                 lock.acquire()
-                queue.put({'service': 'HTTP', 'status': 'DOWN', 'host': target, 'value': value, 'team_num': team_num})
+                queue.put({'service': 'NGINX', 'status': 'DOWN', 'host': target, 'value': value, 'team_num': team_num})
                 lock.release()
 
         # HTTP Server failed to respond
         except requests.exceptions.ConnectionError:
             lock.acquire()
-            queue.put({'service': 'HTTP', 'status': 'DOWN', 'host': target, 'value': value, 'team_num': team_num})
+            queue.put({'service': 'NGINX', 'status': 'DOWN', 'host': target, 'value': value, 'team_num': team_num})
             lock.release()
         except:
             lock.acquire()
-            queue.put({'service': 'HTTP', 'status': 'DOWN', 'host': target, 'value': value, 'team_num': team_num})
+            queue.put({'service': 'NGINX', 'status': 'DOWN', 'host': target, 'value': value, 'team_num': team_num})
             lock.release()
 
         sleep(60)
